a354a4f916fb8d687ecdaf7633fa7b574adcb1d6
vytautas-pilk <vytautas.pilk@yahoo.com>
2018-09-29 14:48:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import PostList from './PostList';
-import './index.css';
+import App from './app';
 import {posts} from './static-data';
-
-class App extends React.Component {
-	state = {
-		posts: this.props.data
-	}
-
-	vote = (index, isUpvote) => {
-		let posts = [...this.state.posts];
-		if (isUpvote) {
-			posts[index].votes += 1;
-		} else {
-			posts[index].votes -= 1;
-		};
-		this.setState({	posts });
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<PostList vote={this.vote} posts={this.state.posts}/>
-			</div>
-		);
-	}
-}
-
-App.propTypes = {
-	data: PropTypes.array.isRequired
-};
 
 ReactDOM.render(<App data={posts}/>, document.querySelector("#root"));
